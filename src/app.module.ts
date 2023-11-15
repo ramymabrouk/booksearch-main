cbe2bf859c0cf603e97e66668e70070498c4718d
ramy mabrouk <ramymabrouk@gmail.com>
2023-11-15 18:45:51 +0200

    some cleanup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { SearchController } from './books/search/search.controller';
 import { SearchService } from './books/search/search.service';
 import { HttpModule } from '@nestjs/axios';
@@ -9,8 +7,8 @@ import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [HttpModule, CacheModule.register()],
-  controllers: [AppController, SearchController],
-  providers: [AppService, SearchService,
+  controllers: [SearchController],
+  providers: [SearchService,
     {
       provide: APP_INTERCEPTOR,
       useClass: CacheInterceptor,
