ba9e227b51e8e28b6625f454f1f167acbd28ba0a
ramy mabrouk <ramymabrouk@gmail.com>
2023-11-15 18:53:02 +0200

    add redis cache

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { SearchService } from './books/search/search.service';
 import { HttpModule } from '@nestjs/axios';
 import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
-  imports: [HttpModule, CacheModule.register()],
+  imports: [HttpModule, CacheModule.register({
+    isGlobal: true,
+    store: redisStore,
+    host: 'cache',
+    port: 6379,
+  }),],
   controllers: [SearchController],
   providers: [SearchService,
     {
